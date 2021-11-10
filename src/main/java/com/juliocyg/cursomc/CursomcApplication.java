0f9c627408c980bb31ceacfd75dd44bf6fca1fce
Julio Cesar <juliocyg.dev@gmail.com>
2021-11-10 11:46:25 -0500

    ¨Producto y Asociacion Muchos para Muchos

--- src/main/java/com/juliocyg/cursomc/CursomcApplication.java
+++ src/main/java/com/juliocyg/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.juliocyg.cursomc.domain.Categoria;
+import com.juliocyg.cursomc.domain.Produto;
 import com.juliocyg.cursomc.repositories.CategoriaRepository;
+import com.juliocyg.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
+
 	@Autowired
-	private CategoriaRepository categoriaRepository; 
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,10 +28,27 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		// Agregado de Categorias
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		// Agregado de Productos
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impresora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		//Asociacion de Categoria y Productos Muchos para Muchos
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 
 }
---
+++ src/main/java/com/juliocyg/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.juliocyg.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juliocyg.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
